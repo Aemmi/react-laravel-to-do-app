b82eb2db6483d71ea17b416b5a55ec98540f0006
Emmanuel Ayeh <imawebservice@gmail.com>
2023-09-22 01:02:08 +0100

    Update Header.jsx

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -20,9 +20,6 @@ function Header()
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
                             <li>
                                 {isLoggedIn ? (
                                 <a className="nav-link" href="#" onClick={handleLogout}>Logout</a>
